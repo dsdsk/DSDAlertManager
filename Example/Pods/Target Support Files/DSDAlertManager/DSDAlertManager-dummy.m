1b7ddd7332e8bb71a292900a6a63bf591a0609c6
dsdsk <s.kumagai.dsd@kumagaisatoshi-no-MacBook-Pro.local>
2016-11-03 14:02:25 +0900

    Git管理するファイルの整理

--- Example/Pods/Target Support Files/DSDAlertManager/DSDAlertManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DSDAlertManager : NSObject
-@end
-@implementation PodsDummy_DSDAlertManager
-@end
--- Example/Pods/Target Support Files/Pods-DSDAlertManager_Tests/Pods-DSDAlertManager_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DSDAlertManager_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_DSDAlertManager_Tests
-@end
